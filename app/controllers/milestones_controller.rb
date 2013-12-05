c00c6898968c2b0bf9cbd3c92695049057bbab22
Darren Cauthon <darren@cauthon.com>
2013-12-04 23:52:10 -0600

    Add widget view.

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,8 +1,26 @@
 class MilestonesController < ApplicationController
+  include ActionView::Helpers::DateHelper
 
   def index
     @milestones = Milestone.for params
     @show_due_column = @milestones.select { |m| m.due_on }.any?
+
+    @panic_table = PanicBoardData::Table.new convert_milestones_to_panic_records(@milestones)
+  end
+
+  private
+
+  def convert_milestones_to_panic_records milestones
+    milestones.map do |milestone|
+
+      progress = milestone.closed_issues.to_f / (milestone.closed_issues + milestone.open_issues)
+      progress = (progress * 8).to_i
+
+      due = milestone.due_on ? time_ago_in_words(milestone.due_on) : ''
+
+      [milestone.title, due, progress_bar_to(progress)]
+
+    end
   end
 
 end
